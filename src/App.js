9a4d8e35f209b22d0fbc7fb683b3abc557db2d9f
Ajin Sunny <ajin.sunny@gmail.com>
2023-11-15 09:49:31 -0600

    (add) cache fix

--- src/App.js
+++ src/App.js
@@ -1,68 +1,61 @@
-
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function App() {
-
-  const[text, setText] = useState("");
-  const[age, setAge] = useState(null);
-  const[gender, setGender] = useState(null);
+  const [text, setText] = useState("");
+  const [age, setAge] = useState(null);
+  const [gender, setGender] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const[cache, setCache] = useState({});
+  const [cache, setCache] = useState({});
 
-  const handleSubmit = async () =>{
-    
-
-    try{
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
       setIsLoading(true);
 
-      if(cache.hasOwnProperty(text)){
-        setAge(cache[text]);
+      if (cache.hasOwnProperty(text)) {
+        setAge(cache[text].age);
         setGender(cache[text].gender);
-        
-      }
-      else{
+      } else {
         const [ageResponse, genderResponse] = await Promise.all([
-          fetch(`https://api.agify.io/?name=${text}`).catch(e => console.log("Error fetching age: ", e)),
-          fetch(`https://api.genderize.io/?name=${text}`).catch(e => console.log("Error fetching gender: ", e))
+          fetch(`https://api.agify.io/?name=${text}`).catch((e) =>
+            console.log("Error fetching age: ", e)
+          ),
+          fetch(`https://api.genderize.io/?name=${text}`).catch((e) =>
+            console.log("Error fetching gender: ", e)
+          ),
         ]);
         const ageValue = await ageResponse.json();
         const genderValue = await genderResponse.json();
         setCache({
           ...cache,
-          [text]: { age: ageValue.age, gender: genderValue.gender }
+          [text]: { age: ageValue.age, gender: genderValue.gender },
         });
-  
+
         setAge(ageValue.age);
         setGender(genderValue.gender);
       }
-
-      
-    }
-    catch{
+    } catch {
       console.log("API Error");
-    }
-    finally{
+    } finally {
       setIsLoading(false);
-      setText("");
     }
+  };
 
-    
-    
-  }
-  
-  return(
-
+  return (
     <div className="page-container flex">
-      <div className = "card flex">
-        <div className="form flex">
-          <input placeholder="Enter Name" value={text} onChange = {(evt)=>setText(evt.target.value)}/>
-          <button onClick={handleSubmit}>Age</button>
-        </div>
+      <div className="card flex">
+        <form onSubmit={handleSubmit} className="form flex">
+          <input
+            placeholder="Enter Name"
+            value={text}
+            onChange={(evt) => setText(evt.target.value)}
+          />
+          <button type="submit">Get Info!</button>
+        </form>
         {isLoading ? <div>Loading...</div> : <div>{age ?? "--"}</div>}
         {isLoading ? <div>Loading...</div> : <div>{gender ?? "--"}</div>}
       </div>
     </div>
-    
-  )
+  );
 }
